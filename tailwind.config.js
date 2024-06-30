82383768e80923b686f23d4e02a3d4319e8828f8
dianprsty <dprasmail@gmail.com>
2024-06-30 17:39:39 +0700

    starter setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: "375px",
+        desktop: "1440px",
+      },
+      colors: {
+        custom: {
+          "almost-white": "hsl(0, 0%, 98%)",
+          "medium-gray": "hsl(0, 0%, 41%)",
+          "almost-black": "hsl(0, 0%, 8%)",
+        },
+      },
+      fontSize: {
+        paragraph: "18px",
+      },
+    },
   },
   plugins: [],
 };
